287a4f5a1c48a85dce835435295dfd44b301a4d2
chaviabrol <chaviabrol@gmail.com>
2022-12-09 02:11:41 +0530

    port

--- index.js
+++ index.js
@@ -82,6 +82,6 @@ app.post('/login',function(req,res){
 });
 
 
-app.listen(PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started successfully!!");
   })
